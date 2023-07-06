8031b6aef65be8bc9d6dde45c7a370b03ab73af0
linakoi <aptgetmain@gmail.com>
2023-07-06 07:29:07 +0000

    asdf

--- seisen_10/10_Traveling.cpp
+++ seisen_10/10_Traveling.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    int a[N][3];
+    long long a[N][3];
     for(int i = 0;i < N;i++) {
         for(int j = 0;j < 3;j++) {
             cin >> a[i][j];
@@ -13,6 +13,15 @@ int main() {
     
     bool can = true;
     bool can_parity = true;
+
+    for(int i = 0;i < N;i++) {
+        for(int j = 0;j < 3;j++) {
+            if(a[i][j] < 0) {
+                break;
+            }
+        }
+    }
+
     //ここで奇数、偶数で判定
     for(int i = 0;i  < N;i++) {
         if(!(a[i][0]%2 == (a[i][1] + a[i][2])%2)) {
@@ -24,7 +33,7 @@ int main() {
     //ここでたどり着けるか判定
     bool can_times = true;
     for(int i = 0;i < N;i++) {
-        if(!(a[i][0] >= a[i][1] + a[i][2]))  {
+        if(!(a[i][0] >= a[i][1] + a[i][2] && ((i!=0)&&(a[i][0]-a[i-1][0] >= abs(a[i][1] - a[i-1][1]) + abs(a[i][2] - a[i-1][2])) || (i == 0))))  {
             can_times = false;
             break;
         }
@@ -37,3 +46,6 @@ int main() {
     }
 
 }
+/*
+
+ */
